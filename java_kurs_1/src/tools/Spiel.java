02e3c51451c5138db904bda70668f6e75a4104ec
sk354-hdm <66474633+sk354-hdm@users.noreply.github.com>
2020-06-08 15:07:36 +0200

    Aufgabe 24 unvollständig

--- java_kurs_1/src/tools/Spiel.java
+++ java_kurs_1/src/tools/Spiel.java
@@ -12,22 +12,21 @@ public abstract class Spiel extends StandardAnwendung implements ActionListener 
 	 protected Timer timer;
 	 
 	 protected Spiel(String titel, int breite,int höhe) {
-		 
 		 super(titel, breite, höhe);
 		 this.setBackground(Color.BLACK);
 		 timer = new Timer(40,this);
-		 initialisiere();
+		 this.initialisiere();
 		 timer.start();
 		 }
 	 
 	 public final void actionPerformed (ActionEvent e) {
-		 neuerSpielstand();
+		 this.neuerSpielstand();
 		 this.repaint();
 	 }
 	 
 	 public final void zeichne(Graphics2D g) {
 		 if(timer.isRunning()) {
-			 zeichneSpielstand(g);
+			 this.zeichneSpielstand(g);
 		 }
 	 }
 	 protected abstract void initialisiere();
