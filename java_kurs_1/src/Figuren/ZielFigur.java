1b05a50c7aeff9548805d160cce0c28397a95b26
sk354-hdm <66474633+sk354-hdm@users.noreply.github.com>
2020-06-10 16:37:45 +0200

    a25

---
+++ java_kurs_1/src/Figuren/ZielFigur.java
@@ -0,0 +1,77 @@
+package Figuren;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.util.Random;
+
+import javax.swing.Timer;
+
+import tools.Klang;
+import tools.Spiel;
+
+public class ZielFigur extends SpielFigur implements ActionListener{
+	
+	private boolean getroffen;
+	private int animationsLänge;
+	private Klang trefferKlang;
+	private Timer tick;
+	private Random zufall;
+		
+	public ZielFigur(double xC, double yC, Spiel spiel) {
+		super(xC, yC, 40, 40, spiel);
+		this.setBewegung(1, 0);
+		animationsLänge = 25;
+		trefferKlang = new Klang("/sound/boing.mp3");
+		tick = new Timer(3000,this);
+		
+		
+	}
+	
+	public void mousePressed (MouseEvent e) {
+		if(this.contains(e.getPoint())) {
+			getroffen = true;
+			trefferKlang.play();
+			tick.start();
+		}
+	}
+	
+	public void zeichne (Graphics2D g) {
+		g.setColor(Color.cyan);
+		if(animationsLänge > 0) {
+		g.fill(this);
+		}
+	}
+	public void bewege() {
+		super.bewege();
+		if(getroffen) {
+			this.width = this.width/1.5;
+			this.height =this.height/1.5;
+			animationsLänge--;
+		}
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		reset();
+	}
+	
+	public void reset () {
+		tick.stop();
+		animationsLänge = 25;
+		this.width = this.height =40;
+		this.x = 20;
+		getroffen = false;
+		tick.setInitialDelay(2000 + zufall.nextInt(4000));
+		this.y = zufall.nextInt(spiel.getHeight()-51)+10;
+		if (y > spiel.getHeight()/2) {
+			this.setBewegung(1, zufall.nextInt(10));
+		}else {
+			this.setBewegung(1, -zufall.nextInt(10));
+		}
+		
+		
+	}
+	
+}
--- java_kurs_1/src/aufgaben/Aufgabe25.java
+++ java_kurs_1/src/aufgaben/Aufgabe25.java
@@ -1,15 +1,38 @@
 package aufgaben;
 
-import Figuren.SpielFigur;
-import tools.Klang;
+import java.awt.Graphics2D;
+
+import Figuren.ZielFigur;
 import tools.Spiel;
 
-public class Aufgabe25 extends SpielFigur {
+public class Aufgabe25 extends Spiel {
+	
+	public static void main(String[] args) {
+		starteAnwendung();
+	}
 	
 	ZielFigur figur1;
 
-	public Aufgabe25(double xC, double yC, Spiel spiel) {
-		super(xC, yC, 40, 40, spiel);
+	public Aufgabe25() {
+		super("Aufgabe 25", 500, 500);
+		
+	}
+
+	@Override
+	protected void initialisiere() {
+		figur1 = new ZielFigur(100, 100, this);
+		
+	}
+
+	@Override
+	protected void neuerSpielstand() {
+		figur1.bewege();
+		
+	}
+
+	@Override
+	protected void zeichneSpielstand(Graphics2D g) {
+		figur1.zeichne(g);
 		
 	}
 	
